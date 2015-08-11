beed3748e4903af2145ad9c5051a043b6176a0e4
Arthur White <arthur@whitedevops.com>
2015-08-11 10:38:52 +0200

    Fix doc

--- doc.go
+++ doc.go
@@ -3,7 +3,7 @@ Volatile is the perfect foundation for any web app as it's designed to have the 
 
 It provides a pure handler (or "middleware") stack so you can perform actions downstream, then filter and manipulate the response upstream.
 
-For a complete documentation, see the [Volatile website](http://volatile.whitedevops.com).
+For a complete documentation, see the Volatile website (http://volatile.whitedevops.com).
 
 Installation
 
@@ -37,6 +37,6 @@ In the terminal:
 
 	$ volatile update
 
-… gets updated versions of the [Volatile Core](https://github.com/volatile/core) and all [official packages](https://github.com/volatile/core#official-handlers).
+… gets updated versions of the Volatile Core (https://github.com/volatile/core) and all official packages (https://github.com/volatile/core#official-handlers).
 */
 package main
