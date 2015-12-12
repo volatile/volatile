6b46cab191fadb345abc6b966b4ad271b5f75baa
Arthur White <arthur@whitedevops.com>
2015-12-12 12:42:04 +0100

    Fix help typo

--- help.go
+++ help.go
@@ -12,12 +12,12 @@ Usage:
 
     volatile [command] [arguments]
 
-With no command, 'volatile' recompiles and reruns the app in the current directory every time a file change.
+Without command, 'volatile' recompiles and reruns the app in the current directory every time a file change.
 
 The commands are:
 
     new [type] [name]  bootstrap and run an app of desired type ('web' or 'api') and name
-    run [args...]      recompile and rerun app with args every time a file change
+    run [arguments]    recompile and rerun app with arguments every time a file change
     update             get updated versions of the Volatile Core and all official packages
 
 `)
